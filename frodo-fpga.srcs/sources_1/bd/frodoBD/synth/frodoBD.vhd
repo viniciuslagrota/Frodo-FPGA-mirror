bb429fde25c27ebf372775f90b4ec32d726a17b0
vinicius <31910262+viniciuslagrota@users.noreply.github.com>
2020-03-29 11:26:30 -0300

    Keccak fast core finalizado e validado.

--- frodo-fpga.srcs/sources_1/bd/frodoBD/synth/frodoBD.vhd
+++ frodo-fpga.srcs/sources_1/bd/frodoBD/synth/frodoBD.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
---Date        : Fri Mar 27 21:52:29 2020
+--Date        : Sat Mar 28 15:28:37 2020
 --Host        : DESKTOP-0F4OK3D running 64-bit major release  (build 9200)
 --Command     : generate_target frodoBD.bd
 --Design      : frodoBD
