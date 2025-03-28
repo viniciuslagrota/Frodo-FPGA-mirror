d163144eb9249ae6a8c40c82acb0a20a3d0c50f2
Vinicius Lagrota <viniciuslagrota@Viniciuss-MacBook-Pro.local>
2025-03-28 15:55:43 +0200

    add: hw and sdk

---
+++ frodo-fpga.sdk/frodo-client/src/include/test_KEM640.c
@@ -0,0 +1,11 @@
+/*
+ * test_KEM640.c
+
+
+ *
+ *  Created on: 1 de dez de 2019
+ *      Author: Vinicius
+ */
+
+
+//#include "test_kem.c"
---
+++ frodo-fpga.sdk/frodo_sdk/src/include/kem.h
@@ -0,0 +1,20 @@
+/*
+ * kem.h
+ *
+ *  Created on: 1 de dez de 2019
+ *      Author: Vinicius
+ */
+
+#ifndef SRC_INCLUDE_KEM_H_
+#define SRC_INCLUDE_KEM_H_
+
+#include "global_def.h"
+
+#define LE_TO_UINT16(n) (n)
+#define UINT16_TO_LE(n) (n)
+
+int crypto_kem_keypair(unsigned char* pk, unsigned char* sk);
+int crypto_kem_enc(unsigned char *ct, unsigned char *ss, const unsigned char *pk);
+int crypto_kem_dec(unsigned char *ss, const unsigned char *ct, const unsigned char *sk);
+
+#endif /* SRC_INCLUDE_KEM_H_ */
